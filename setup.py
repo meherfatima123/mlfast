beb3707f9061085d1037b4b5c575a30fb97ad292
Meher Fatima <86418626+meherfatima123@users.noreply.github.com>
2023-09-08 04:12:39 -0700

    setup.py
    updating setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 from setuptools import setup, find_packages
 import codecs
-import os   
+import os
 
 # Get the directory of the current script
 here = os.path.abspath(os.path.dirname(__file__))
@@ -24,3 +24,19 @@ REPO_NAME = "mlfast"
 AUTHOR_USER_NAME = "Abdul-Jaweed"
 SRC_REPO = "mlfast"
 AUTHOR_EMAIL = "jdgaming7320@gmail.com"
+
+setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="It's a Python machine learning package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
